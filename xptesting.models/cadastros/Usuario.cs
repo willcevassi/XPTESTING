03d7cb6d59eef3c75048981f52091c6fd88194dc
Willian Cevassi <willian.cevassi@outlook.com.br>
2017-02-21 11:26:38 -0300

    Criação da classe Usuário

---
+++ xptesting.models/cadastros/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xptesting.models.cadastros
+{
+    public class Usuario
+    {
+        public long UsuarioId { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
